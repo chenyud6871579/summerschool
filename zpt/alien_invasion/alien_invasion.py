c4f4a339956c3954cae977c9b4d9f8f6524b2f94
Zpt <1633001347@qq.com>
2020-06-18 20:02:47 +0800

    6.18 Alien

--- zpt/alien_invasion/alien_invasion.py
+++ zpt/alien_invasion/alien_invasion.py
@@ -1,14 +1,36 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
+
+
 def run_game():
     #创建屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width,ai_settings.screen_height)
+    )
     pygame.display.set_caption("Alien Invasion")
+    #背景颜色
+    bg_color = (230,230,230)
 
+    #创建飞船
+    ship = Ship(ai_settings,screen)
+
+    #创建一个用于存储子弹的编组
+    bullets = Group()
+
+    #主循环
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        pygame.display.flip()
+        #监听事件
+        gf.check_events(ai_settings,screen,ship,bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings,screen,ship,bullets)
+        
+
+#开始展示
 run_game()
