79cc40f97bf0757f32382a09bfeeb0e2c1031c78
863320295 <863320295@qq.com>
2020-07-12 22:10:00 +0800

    小组项目更新

--- teamwork/douban-thrift-python-service/thrift/douban.thrift
+++ teamwork/douban-thrift-python-service/thrift/douban.thrift
@@ -1,1 +1,13 @@
+# 声明 Java 的包名
 namespace java com.test.thrift.douban
+# 声明 Python 的包名
+namespace py douban.api
+
+# 定义服务，给出接口名称
+service DoubanService
+{
+    # 服务1(待定)
+    bool spider1()
+    # 服务2(待定)
+    bool spider2()
+}
