087ab6713c20524a72c88206b89a896c4aca63ca
Zpt <1633001347@qq.com>
2020-07-30 16:44:59 +0800

    调整 git ignore

--- CDA/mapreduce-thrift-service/src/test/java/RequestTest.java
+++ CDA/mapreduce-thrift-service/src/test/java/RequestTest.java
@@ -1,12 +1,7 @@
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.file.Path;
 
 public class RequestTest {
 
